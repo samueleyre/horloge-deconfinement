fb85ec6440bedf5a927d337e8fe457821dfd717a
xalava <xavier.lavayssiere@gmail.com>
2020-04-14 00:39:16 +0200

    premiere horloge

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Horloge deconfinement
+
+Il s'agit d'horloges collaboratives. Pour ajouter la votre, il suffit d'ajouter un dossier avec une version de l'horloge et le nom de ce dossier dans le tableau de index.html.
---
+++ xavier/script.js
@@ -0,0 +1,19 @@
+function calculDiff() {
+    let deconfinement = new Date(2020, 05, 11, 00, 00, 0)
+    let maintenant = new Date()
+    let diff = {}
+    diff.total = deconfinement - maintenant
+    diff.jours = Math.floor(diff.total/(1000*60*60*24))
+    diff.heures = Math.floor((diff.total/(1000*60*60)) % 24)
+    diff.secondes = Math.floor( (diff.total/1000) % 60 );
+    return diff
+    
+}
+d = calculDiff()
+document.querySelector("#affichage").innerHTML = `Il reste ${d.jours} jours, ${d.heures} heures et ${d.secondes} secondes`
+
+setInterval(() => {
+    d = calculDiff()
+    document.querySelector("#affichage").innerHTML = `Il reste ${d.jours} jours, ${d.heures} heures et ${d.secondes} secondes`
+}, 1000);
+
