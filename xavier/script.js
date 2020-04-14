69abcf96513df47535881ab5f5466cdab930c842
xalava <xavier.lavayssiere@gmail.com>
2020-04-14 14:44:34 +0200

    Ajout ane

--- xavier/script.js
+++ xavier/script.js
@@ -5,15 +5,37 @@ function calculDiff() {
     diff.total = deconfinement - maintenant
     diff.jours = Math.floor(diff.total/(1000*60*60*24))
     diff.heures = Math.floor((diff.total/(1000*60*60)) % 24)
-    diff.secondes = Math.floor( (diff.total/1000) % 60 );
+    diff.minutes = Math.floor((diff.total/(1000*60)) % 60)
+
+    diff.secondes = Math.floor( (diff.total/1000) % 60 )
     return diff
-    
+
 }
+function fix(num) {
+    return num.toString().padStart(2, 0);
+}
+
 d = calculDiff()
-document.querySelector("#affichage").innerHTML = `Il reste ${d.jours} jours, ${d.heures} heures et ${d.secondes} secondes`
+document.querySelector("#affichage").innerHTML = `Il reste ${d.jours} jours, ${d.heures} heures, ${fix(d.minutes)} minutes et ${fix(d.secondes)} secondes`
 
 setInterval(() => {
     d = calculDiff()
-    document.querySelector("#affichage").innerHTML = `Il reste ${d.jours} jours, ${d.heures} heures et ${d.secondes} secondes`
+    document.querySelector("#affichage").innerHTML = `Il reste ${d.jours} jours, ${d.heures} heures, ${fix(d.minutes)} minutes et ${fix(d.secondes)} secondes`
+
 }, 1000);
 
+setTimeout(() => {
+    document.querySelector("#imotep").removeAttribute("hidden")
+}, 2000);  
+
+setTimeout(() => {
+
+    setInterval(() => {
+        
+        document.querySelector("#alternative").innerHTML = `Ou bien ${Math.round(Math.random()*72)+27} jours, ${fix(Math.round(Math.random()*24))} heures, ${fix(Math.round(Math.random()*24))} minutes et ${fix(Math.round(Math.random()*60))} secondes `
+    }, 1200);
+    
+    setTimeout(() => {
+        document.querySelector("#ane").removeAttribute("hidden")
+    }, 5000);  
+}, 3000);
